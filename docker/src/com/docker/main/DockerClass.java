620f544ae4a2b8a4d05ba4f776d87c09e437d392
Rajnish <rajnishsasmal@gmail.com>
2021-05-22 15:47:59 +0530

    space change

--- docker/src/com/docker/main/DockerClass.java
+++ docker/src/com/docker/main/DockerClass.java
@@ -6,7 +6,7 @@ public class DockerClass {
 		
 	System.out.println("Hello GIT");
 	System.out.println("Change in GIT");
-        System.out.println("Change by JOHN from git");
+    System.out.println("Change by JOHN from git");
 	
 		
 	}
